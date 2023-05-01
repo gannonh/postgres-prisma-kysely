1f8b00c9adb89d4041834b42c4f06f83ccf50c49
gannonh <gannonh@users.noreply.github.com>
2023-05-01 12:00:48 -0700

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { Suspense } from 'react'
-import Table from '@/components/table'
-import TablePlaceholder from '@/components/table-placeholder'
-import ExpandingArrow from '@/components/expanding-arrow'
+import Image from "next/image";
+import Link from "next/link";
+import { Suspense } from "react";
+import Table from "@/components/table";
+import TablePlaceholder from "@/components/table-placeholder";
+import ExpandingArrow from "@/components/expanding-arrow";
 
 // Prisma does not support Edge without the Data Proxy currently
-export const runtime = 'nodejs' // default
-export const preferredRegion = 'home'
-export const dynamic = 'force-dynamic'
+export const runtime = "nodejs"; // default
+export const preferredRegion = "home";
+export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-center">
+    <main className="relative flex flex-col items-center justify-center min-h-screen">
       <Link
-        href="https://vercel.com/templates/next.js/postgres-prisma"
-        className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
+        href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fgannonh%2Fpostgres-prisma-kysely"
+        className="flex px-10 py-2 mt-20 space-x-1 text-sm font-medium text-gray-600 transition-all rounded-full shadow-sm group sm:mt-0 bg-white/30 ring-1 ring-gray-900/5 hover:shadow-lg active:shadow-sm"
       >
         <p>Deploy your own to Vercel</p>
         <ExpandingArrow />
@@ -27,46 +27,38 @@ export default function Home() {
         {/* @ts-expect-error Async Server Component */}
         <Table />
       </Suspense>
-      <p className="font-light text-gray-600 w-full max-w-lg text-center mt-6">
+      <p className="w-full max-w-lg mt-6 font-light text-center text-gray-600">
         <Link
           href="https://vercel.com/postgres"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
+          className="font-medium underline transition-colors underline-offset-4 hover:text-black"
         >
           Vercel Postgres
-        </Link>{' '}
-        demo with{' '}
+        </Link>{" "}
+        demo with{" "}
         <Link
           href="https://prisma.io"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
+          className="font-medium underline transition-colors underline-offset-4 hover:text-black"
         >
           Prisma
-        </Link>{' '}
-        as the ORM. <br /> Built with{' '}
+        </Link>{" "}
+        as the schema mnaager and{" "}
         <Link
-          href="https://nextjs.org/docs"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Next.js App Router
-        </Link>
-        .
-      </p>
-
-      <div className="flex justify-center space-x-5 pt-10 mt-10 border-t border-gray-300 w-full max-w-xl text-gray-600">
-        <Link
-          href="https://postgres-starter.vercel.app/"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Starter
-        </Link>
-        <Link
-          href="https://postgres-kysely.vercel.app/"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
+          href="https://kysely.dev/"
+          className="font-medium underline transition-colors underline-offset-4 hover:text-black"
         >
           Kysely
-        </Link>
-      </div>
+        </Link>{" "}
+        as the db client, using{" "}
+        <Link
+          href="https://github.com/valtyr/prisma-kysely"
+          className="font-medium underline transition-colors underline-offset-4 hover:text-black"
+        >
+          prisma-kysely
+        </Link>{" "}
+        to bridge the two.
+      </p>
 
-      <div className="sm:absolute sm:bottom-0 w-full px-20 py-10 flex justify-between">
+      <div className="flex justify-between w-full px-20 py-10 sm:absolute sm:bottom-0">
         <Link href="https://vercel.com">
           <Image
             src="/vercel.svg"
@@ -77,7 +69,7 @@ export default function Home() {
           />
         </Link>
         <Link
-          href="https://github.com/vercel/examples/tree/main/storage/postgres-prisma"
+          href="https://github.com/gannonh/postgres-prisma-kysely"
           className="flex items-center space-x-2"
         >
           <Image
@@ -91,5 +83,5 @@ export default function Home() {
         </Link>
       </div>
     </main>
-  )
+  );
 }
